2750dc84bda6ba1299c2482f174a9d590c65e1d7
itbeauregard <i.t.beauregard@gmail.com>
2017-08-03 15:02:02 -0400

    Create resource watchers
    Add new resource-watcher file and implement it in the ui.js to connect
    its functions with onJoinGameSuccess. Don't quite understand how it
    works. Fetched code from someone else's issue:
    (https://git.generalassemb.ly/ga-wdi-boston/game-project/issues/361)

--- assets/scripts/accounts/ui.js
+++ assets/scripts/accounts/ui.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const app = require('../store.js')
+const resourceWatcher = require('./resource-watcher-0.1.0.js')
+const config = require('../config.js')
 
 const onSuccess = function (data) {
   app.user = data.user
@@ -44,14 +46,45 @@ const onGetGameSuccess = function (data) {
   console.log('onGetGameSuccess from ui.js ran!')
 }
 
-const onJoinGameSuccess = function (data) {
-  app.data = data.user
-  console.log(data)
-  console.log('onJoinGameSuccess from ui.js ran!')
+const onJoinGameSuccess = (data) => {
+  app.user = data.user
+  app.game = data.game
+  $('.player2-message').text('Successfully joined')
+  $('.player2-game').text('Game ID: ' + app.game.id)
+
+  const gameWatcher = resourceWatcher.resourceWatcher(config.host + '/' + app.game.id + '/watch', {
+    Authorization: 'Token token=' + app.user.token
+  })
+
+  gameWatcher.on('change', function (data) {
+    console.log(data)
+    if (data.game && data.game.cells) {
+      const diff = changes => {
+        const before = changes[0]
+        const after = changes[1]
+        for (let i = 0; i < after.length; i++) {
+          if (before[i] !== after[i]) {
+            return {
+              index: i,
+              value: after[i]
+            }
+          }
+        }
+
+        return { index: -1, value: '' }
+      }
+
+      const cell = diff(data.game.cells)
+      $('#watch-index').val(cell.index)
+      $('#watch-value').val(cell.value)
+    } else if (data.timeout) { // not an error
+      gameWatcher.close()
+    }
+  })
 }
 
 const onUpdateGameStateSuccess = function (data) {
-  app.data = data.user
+  app.user = data.user
   console.log(data)
   console.log('onJoinGameSuccess from ui.js ran!')
 }
